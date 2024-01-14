7d4333d312578a5799dfb0ac7018ab297bf82896
sally <spoovathumkadavil16@claytonschools.net>
2024-01-13 18:19:12 -0600

    Started The Cleaning Process

--- src/main/java/frc/robot/commands/swervedrive/drivebase/SwerveCommand.java
+++ src/main/java/frc/robot/commands/swervedrive/drivebase/SwerveCommand.java
@@ -1,29 +1,14 @@
 package frc.robot.commands.swervedrive.drivebase;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
-import frc.robot.subsystems.swervedrive.SwerveSubsystem;
 
+/**
+ * The only goal of this command is to move either robot
+ * or angle centric
+ */
 public class SwerveCommand extends CommandBase {
 
-  private CommandJoystick joystick;
-  private SwerveSubsystem swerve;
-
-  private final double MAX_FWD_SENS = 4;
-  private final double MAX_STR_SENS = 4;
-  private final double MAX_ROT_SENS = 2;
-
-  public SwerveCommand(SwerveSubsystem drive, CommandJoystick joystick) {
-    this.joystick = joystick;
-    this.swerve = drive;
-    addRequirements(swerve);
+  public SwerveCommand() {
   }
 
-  @Override
-  public void execute() {
-
-    double forward = joystick.getY() * MAX_FWD_SENS;
-    double sideways = joystick.getX() * MAX_STR_SENS;
-    double rotation = joystick.getZ() * MAX_ROT_SENS;
-  }
 }
